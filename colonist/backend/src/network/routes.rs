38b6ed1be243f6cf48693a729b8e83b5d90c9d77
Adpavelka <adpavelka@seznam.cz>
2026-02-08 18:38:37 +0100

    refactor: the only time I touch network and repository

--- colonist/backend/src/network/routes.rs
+++ colonist/backend/src/network/routes.rs
@@ -11,14 +11,10 @@ pub struct AuthQuery {
     pub id: Option<Uuid>,
 }
 #[get("/ws")]
-pub async fn ws_index(
-    req: HttpRequest,
-    stream: web::Payload,
-    lobby_addr: web::Data<Addr<Lobby>>,
-    query: web::Query<AuthQuery>
-) -> Result<HttpResponse, Error> {
+pub async fn ws_index(req: HttpRequest, stream: web::Payload, lobby_addr: web::Data<Addr<Lobby>>, query: web::Query<AuthQuery>) -> Result<HttpResponse, Error> {
     let player_id = query.id.unwrap_or_else(Uuid::new_v4);
     info!("player_id: {}", player_id);
+    
     let worker = WsWorker::new(player_id, lobby_addr.get_ref().clone());
     ws::start(worker, &req, stream)
 }
